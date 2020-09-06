772bd44a35a57d9ee2bc27fe944a29fa5dff5d1c
tiking76 <716yosiki1999@gmail.com>
2020-09-07 00:26:26 +0900

    :sparkles:Messageのデータ更新

--- chatApp/Model/Message.swift
+++ chatApp/Model/Message.swift
@@ -6,9 +6,23 @@
 //  Copyright © 2020 Yoshiki Tachi. All rights reserved.
 //
 
-import Foundation
+import Firebase
 
 struct Message {
     let text: String
+    let toId : String
+    let fromId : String
+    var timeStamp: Timestamp!
+    var user: User?
+    
     let isFromCurrentUser : Bool
+    
+    init(dictinary: [String: AnyObject]) {
+        self.text = dictinary["text"] as? String ?? ""
+        self.toId = dictinary["toId"] as? String ?? ""
+        self.fromId = dictinary["fromId"] as? String ?? ""
+        self.timeStamp = dictinary["timestamp"] as? Timestamp ?? Timestamp.init(date: Date())
+        
+        self.isFromCurrentUser = fromId == Auth.auth().currentUser?.uid
+    }
 }
